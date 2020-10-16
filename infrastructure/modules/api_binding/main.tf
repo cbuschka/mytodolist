eb5ae6f650d40b6db10ebf4b95863ac219abf5a6
conni <cbuschka@gmail.com>
2020-10-16 14:18:30 +0200

    Changed request template.

--- infrastructure/modules/api_binding/main.tf
+++ infrastructure/modules/api_binding/main.tf
@@ -20,18 +20,16 @@ resource "aws_api_gateway_integration" "request_method_integration" {
     "application/json" = <<EOF
 #set($allParams = $input.params())
 {
-  "params" : {
-    #foreach($type in $allParams.keySet())
-    #set($params = $allParams.get($type))
-    "$type" : {
-      #foreach($paramName in $params.keySet())
-      "$paramName" : "$util.escapeJavaScript($params.get($paramName))"
-      #if($foreach.hasNext),#end
-      #end
-    }
+  #foreach($type in $allParams.keySet())
+  #set($params = $allParams.get($type))
+  "$type" : {
+    #foreach($paramName in $params.keySet())
+    "$paramName" : "$util.escapeJavaScript($params.get($paramName))"
     #if($foreach.hasNext),#end
     #end
   }
+  #if($foreach.hasNext),#end
+  #end
 }
 EOF
   }
